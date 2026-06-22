fcf171bffa5a755e1f6fc7d61bba99d4ceee5316
ARYAN7172 <tiwariaryan2324@gmail.com>
2026-06-22 21:12:49 +0530

    Week 3 till day 3 work done

--- Week_2/Day_5/Problem3.cpp
+++ Week_2/Day_5/Problem3.cpp
@@ -6,7 +6,7 @@ public:
 
         for (int i = 0; i < n; i++) {
 
-            // Odd length palindrome
+            
             int left = i, right = i;
             while (left >= 0 && right < n && s[left] == s[right]) {
                 if (right - left + 1 > maxLen) {
@@ -17,7 +17,7 @@ public:
                 right++;
             }
 
-            // Even length palindrome
+          
             left = i;
             right = i + 1;
             while (left >= 0 && right < n && s[left] == s[right]) {
