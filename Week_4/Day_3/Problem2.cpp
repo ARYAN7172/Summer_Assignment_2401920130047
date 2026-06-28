3915a093301cc5561e270860f2dfad939627f73a
ARYAN7172 <tiwariaryan2324@gmail.com>
2026-06-28 22:03:14 +0530

    Week 4 day 3 and 4 work done

---
+++ Week_4/Day_3/Problem2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool check(TreeNode* root, long long mn, long long mx) {
+        if (root == NULL)
+            return true;
+
+        if (root->val <= mn || root->val >= mx)
+            return false;
+
+        return check(root->left, mn, root->val) &&
+               check(root->right, root->val, mx);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return check(root, LLONG_MIN, LLONG_MAX);
+    }
+};
